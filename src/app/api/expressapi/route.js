77a53fd1f0379a1d6025a592bb5980615aa24f63
anildalar <dollorinfotech@gmail.com>
2024-05-19 18:01:24 +0530

    new

--- src/app/api/expressapi/route.js
+++ src/app/api/expressapi/route.js
@@ -1,11 +1,29 @@
 export async function POST(req,res) {
     const { phoneNumber,tweetData,hashTags,fileUrl } = await req.json()
-    /* const res = await fetch(`${process.env.EXPRESS_API}/test`, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-    const data = await res.json() */
+    //phoneNumber,tweetData,tag,fileUrl
+
+    const baseUrl = `${process.env.EXPRESS_API}/tweet`;
+    const url = new URL(baseUrl);
+
+    url.searchParams.append('phoneNumber', phoneNumber);
+    url.searchParams.append('tweetData', tweetData);
+    url.searchParams.append('tag', hashTags);
+    url.searchParams.append('fileUrl', fileUrl);
+
+    try {
+      const res = await fetch(url.toString(), {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const data = await res.json();
+      console.log('Success:', data);
+      return Response.json({ 'status':'Success',data })
+    } catch (error) {
+      console.error('Error:', error);
+      return Response.json({ 'status':'Error',error  })
+    }
    
-    return Response.json({ phoneNumber,tweetData,hashTags,fileUrl })
+    
 }
