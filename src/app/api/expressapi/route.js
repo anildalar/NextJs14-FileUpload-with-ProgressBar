f5470d311b585e909b9e893d27c0c40408a6a3d7
anildalar <dollorinfotech@gmail.com>
2024-05-20 15:27:12 +0530

    ROhit

--- src/app/api/expressapi/route.js
+++ src/app/api/expressapi/route.js
@@ -1,33 +1,40 @@
-export async function POST(req,res) {
-    const { phoneNumber,tweetData,hashTags,fileUrl } = await req.json()
-    //phoneNumber,tweetData,tag,fileUrl
+export async function POST(req, res) {
+  try {
+    const { phoneNumbers, tweetData, hashTags, fileUrl } = await req.json();
+    const phoneNumberList = phoneNumbers; // Assuming phoneNumbers is an array of strings
 
     const baseUrl = `${process.env.EXPRESS_API}/tweet`;
-    const url = new URL(baseUrl);
 
-    url.searchParams.append('phoneNumber', phoneNumber);
-    url.searchParams.append('tweetData', tweetData);
-    url.searchParams.append('tag', hashTags);
-    url.searchParams.append('fileUrl', fileUrl);
+    const requests = phoneNumberList.map(async (phoneNumber) => {
+      const url = new URL(baseUrl);
 
-    try {
-      const res = await fetch(url.toString(), {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-      const data = await res.json();
-      if (data.error) {
-        console.error('Error:', data.error);
-        return Response.json({ status: 'Error', error: data.error });
+      url.searchParams.append('phoneNumber', phoneNumber.trim());
+      url.searchParams.append('tweetData', tweetData);
+      url.searchParams.append('tag', hashTags);
+      url.searchParams.append('fileUrl', fileUrl);
+
+      try {
+        const response = await fetch(url.toString(), {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+
+        const data = await response.json();
+
+        return { phoneNumber, status: response.status, data };
+      } catch (error) {
+        console.error('Error:', error);
+        return { phoneNumber, status: 'Error', error: error.message };
       }
-      console.log('Success:', data);
-      return Response.json({ 'status':'Success',data })
-    } catch (error) {
-      console.error('Error:', error);
-      return Response.json({ 'status':'Error',error  })
-    }
-   
-    
+    });
+
+    const results = await Promise.all(requests);
+
+    return res.json({ status: 'Complete', results });
+  } catch (error) {
+    console.error('Error:', error);
+    return res.json({ status: 'Error', error: error.message });
+  }
 }
